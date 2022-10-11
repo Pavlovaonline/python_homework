8cfd61232e11f0574907c6ce662f7b6313462c06
Ekaterina <katekeehl@yandex.ru>
2022-10-11 23:43:42 +0300

    homework01

---
+++ lesson01/exercise04.py
@@ -0,0 +1,29 @@
+from pickle import NONE
+
+try: 
+    a = float(input("Введите первое число: "))
+    b = float(input("Введите второе число: "))
+    operation = input("Введите операцию: ")
+    if operation == "+": 
+        c = a+b
+    elif operation == "-": 
+        c = a-b
+    elif operation == "*": 
+        c = a*b
+    elif operation == "/":
+        if b == 0: 
+            print("Делить на 0 нельзя!")
+        else: 
+            c = a/b
+    elif operation == "mod": 
+        c=a%b
+    elif operation == "pow": 
+        c=a**b
+    elif operation == "div": 
+        if b == 0: 
+            print("Делить на 0 нельзя!")
+        else: 
+            c = a//b
+    print(round(c, 3))
+except: print("Что-то пошло не так...")
+
