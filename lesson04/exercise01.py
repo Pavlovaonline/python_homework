3e7ed75ccaa579143eb0c28a72c391eeb4a5441c
Ekaterina <katekeehl@yandex.ru>
2022-10-21 03:29:57 +0300

    lesson03

---
+++ lesson04/exercise01.py
@@ -0,0 +1,15 @@
+# a = int(input())
+# b = int(input())
+# if a<b:
+#     min_dig = a
+#     max_dig = b
+# elif a>b:
+#     min_dig = b
+#     max_dig = a
+# elif a==b:
+#     min_dig = a
+#     max_dig = b
+# for i in range(2, min_dig):
+#     if max_dig%i==0:
+#         print("делим на ", i)
+#         break
