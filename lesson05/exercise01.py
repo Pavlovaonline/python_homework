27ebe53d9cef1bc57dafbe9ac5e37e725896f1a3
Ekaterina <katekeehl@yandex.ru>
2022-10-27 01:16:24 +0300

    lesson05_v0

--- lesson05/exercise01.py
+++ lesson05/exercise01.py
@@ -1,15 +1,47 @@
-a = int(input())
-b = int(input())
-if a<b:
-    min_dig = a
-    max_dig = b
-elif a>b:
-    min_dig = b
-    max_dig = a
-elif a==b:
-    min_dig = a
-    max_dig = b
-for i in range(2, min_dig):
-    if max_dig%i==0:
-        print("делим на ", i)
-        break
+# from curses.ascii import isdigit
+from random import randint
+from turtle import st
+
+candies = 2021
+max_step = 28
+print("Правила:\n\tЗа один ход вы можете взять не менее 0 и не более 28 конфет\n\tПобеждает тот, кто забирает последнюю конфету.")
+
+i = randint(1, 2)
+
+def human_action (): 
+    try:
+        # step = int(input("Сколько конфет вы хотите взять? "))
+        step = randint(0, 28)
+        print(f"Вы взяли {step} конфет")
+        # if not isdigit(step):
+        #     print("Введите целое число.")
+        #     human_action
+        if step < 0 or step > max_step:
+            print ("Вы можете взять не больше 28 конфет.")
+            human_action
+        if step <= 0 or step >= max_step:
+            candies -= step
+        print(f"Осталось {candies} конфет.")
+    except: print("Что-то не так.")
+
+def bot_action ():
+    try:
+        if i == 2:
+            candies -= 0
+        elif i == 1:
+            if candies == 2021:
+                step = candies - (candies % (max_step+1))
+                print(f"Бот взял {step} конфет")
+            elif candies >= (max_step+1):
+                step = max_step - (candies % (max_step+1))
+                print(f"Бот взял {step} конфет")
+            elif candies <= (max_step+1):
+                step = candies - 1
+                print(f"Бот взял {step} конфет")
+            candies -= step
+    except: print("Что-то не так.")
+
+while candies < 0:
+    print(bot_action())
+    print(human_action())
+    print("Game over")
--- lesson05/exercise02.py
+++ lesson05/exercise02.py
@@ -1,1 +1,37 @@
-del_abc_sp
+with open("C:/GB/Python/homework/lesson05/originaltext.txt", "r") as file: 
+    original_text = file.read()
+
+with open("C:/GB/Python/homework/lesson05/RLE_code.txt", "r") as file: 
+     decode_text = str(file.read())
+
+def rle_encode (text):
+    code = ""
+    symbol = ""
+    count = 1
+    for char in text:
+        if char != symbol:
+            if symbol:
+                code += str(count) + str(symbol) 
+            count = 1
+            symbol = char
+        else:
+            count += 1
+    else: 
+        code += str(count) + str(symbol)
+    with open("C:/GB/Python/homework/lesson05/RLE_code.txt", "w") as file: 
+        file.write(code)
+    return code
+
+def rle_decode (text):
+    code = ""
+    count = ""
+    for char in text:
+        if char.isdigit():
+            count += char
+        else: 
+            code += char * int(count)
+            count = ""
+    return code
+
+print(rle_encode(original_text))
+print(rle_decode(decode_text))
